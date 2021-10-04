a2fbf28512ad8e73c79c55d2ca417d77f691a550
USBDRIVE <adammclean265@gmail.com>
2021-10-03 20:27:14 -0700

    Base Project

---
+++ GoLogData/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoLogData.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
