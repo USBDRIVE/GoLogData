1b95df863ece81a019545135d6c25bff43d87e0c
USBDRIVE <adammclean265@gmail.com>
2021-10-07 12:38:47 -0700

    Added Databook class and update database

---
+++ GoLogData/Data/Migrations/20211007193743_databookclass.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoLogData.Data.Migrations
+{
+    public partial class databookclass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Databook",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Databook",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Databook",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Databook");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Databook");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Databook");
+        }
+    }
+}
