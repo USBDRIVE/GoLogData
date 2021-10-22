ce2ca395e9d3a07ef88814169df74765c4f8fc80
USBDRIVE <adammclean265@gmail.com>
2021-10-22 13:17:49 -0700

    Added DataModel class and started edit page of databooks

--- GoLogData/Models/Databooks.cs
+++ GoLogData/Models/Databooks.cs
@@ -1,10 +1,23 @@
-﻿namespace GoLogData.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GoLogData.Models
 {
     public class Databooks
     {
+        [Key]
         public Guid Id { get; set;  }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Color { get; set; }
+    }
+    public class DataModel
+    {
+        //connect an id here to the guid in databooks class
+        [ForeignKey("DatabookId")]
+        public Databooks Databooks { get; set; }
+
+        public string Title{  get; set; }
+        public string Description {  get; set; }
     }
 }   
